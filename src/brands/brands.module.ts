8e58c4a0546406c059442a6dc81288794e97fc12
ygcristopher <ygca.dev@gmail.com>
2026-06-02 17:40:34 -0300

    feat: update BrandsModule to include BrandsController and BrandsService

--- src/brands/brands.module.ts
+++ src/brands/brands.module.ts
@@ -2,8 +2,12 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 
 import { Brand } from '../database/entities/brand.entity';
+import { BrandsController } from './brands.controller';
+import { BrandsService } from './brands.service';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Brand])],
+  controllers: [BrandsController],
+  providers: [BrandsService],
 })
 export class BrandsModule {}
